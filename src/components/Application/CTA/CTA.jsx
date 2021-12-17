41e8f7f3ad4fbb32d9c10e5d3c49005ce6648e65
Daniel Yeh <daniel548604106@gmail.com>
2021-12-18 01:56:38 +0800

    save

--- src/components/Application/CTA/CTA.jsx
+++ src/components/Application/CTA/CTA.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Button from '../../Global/Button/Button.jsx';
 import classes from './CTA.module.scss';
+
 const CTA = () => {
   return (
     <div className={classes.ctaLayout}>
