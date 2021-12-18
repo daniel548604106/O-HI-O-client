06dc5aea695f87899e58f23aa0cf51df1043ba90
Daniel Yeh <daniel548604106@gmail.com>
2021-12-18 12:44:52 +0800

    url

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,10 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL:
-    process.env === 'production'
-      ? 'https://o-hi-o-server.de.r.appspot.com/'
-      : 'http://localhost:3000',
+  baseURL: 'https://o-hi-o-server.de.r.appspot.com/',
 });
 
 export default axiosInstance;
