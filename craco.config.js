3d352cd1bab5d71c2d906cd7b2d160503ec19ce4
Daniel Yeh <daniel548604106@gmail.com>
2022-01-06 23:17:52 +0800

    added craco extract critical css

--- craco.config.js
+++ craco.config.js
@@ -1,9 +1,26 @@
 const path = require('path');
+const HtmlCriticalWebpackPlugin = require('html-critical-webpack-plugin');
 
 module.exports = {
   webpack: {
     alias: {
       '@/Components': path.resolve(__dirname, 'src/components/'),
     },
+    mode: 'production',
+    plugins: [
+      new HtmlCriticalWebpackPlugin({
+        base: path.resolve(__dirname, 'build'),
+        src: 'index.html',
+        dest: 'index.html',
+        inline: true,
+        minify: true,
+        extract: true,
+        width: 375,
+        height: 565,
+        penthouse: {
+          blockJSRequests: false,
+        },
+      }),
+    ],
   },
 };
